8b5851a71104fe9f3245e4ad241a7785028b1779
yoZing <josephkim525@gmail.com>
2024-03-04 00:14:31 +0900

    20240304 작업자 : 김요셉 작업내용 : 데이터 통신 규격 예제 생성

--- src/main/java/klj/project/web/controller/LoginController.java
+++ src/main/java/klj/project/web/controller/LoginController.java
@@ -2,11 +2,14 @@ package klj.project.web.controller;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import klj.project.service.LoginService;
+import klj.project.web.dto.KljResponse;
 import klj.project.web.dto.Login.oauth.NaverOauthRequestDto;
 import klj.project.web.dto.Login.oauth.OauthTokenDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +20,16 @@ public class LoginController {
 
     @PostMapping("/login/callback/naver")
     @CrossOrigin("http://localhost:3000")
-    public String getNaverAuth(@RequestBody NaverOauthRequestDto naverOauthRequestDto) {
+    public KljResponse<String> getNaverAuth(@RequestBody NaverOauthRequestDto naverOauthRequestDto) {
         String code =naverOauthRequestDto.getCode();
         String state =naverOauthRequestDto.getState();
         String url = "";
         OauthTokenDto oauthTokenDto = loginService.getNaverAccessToken(code,state);
         String oauthId = loginService.getNaverInfo(oauthTokenDto);
-        return url;
+
+        KljResponse<String> buildWith = null;
+        buildWith = KljResponse.create().succeed().buildWith(oauthId);
+
+        return buildWith;
     }
 }
