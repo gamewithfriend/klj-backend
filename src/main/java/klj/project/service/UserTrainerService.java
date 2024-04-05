9724aa430c985c67daa2d132ddab0c3f22eebc65
gamewithfriend <jse3685545@naver.com>
2024-04-05 16:08:36 +0900

    jth 240405 트레이너 신청 이메일 발송 및 유저 개인정보 변경 업데이트

--- src/main/java/klj/project/service/UserTrainerService.java
+++ src/main/java/klj/project/service/UserTrainerService.java
@@ -1,13 +1,12 @@
 package klj.project.service;
 
-import klj.project.domain.user.Authority;
-import klj.project.domain.user.OauthType;
 import klj.project.domain.user.Trainer;
 import klj.project.domain.user.User;
 import klj.project.repository.UserRepository;
 import klj.project.repository.UserTrainerRepository;
 import klj.project.strategyPattern.context.NoticeContext;
-import klj.project.strategyPattern.strategy.sendInnerNoticeAdmin;
+import klj.project.strategyPattern.strategy.SendInnerNoticeAdmin;
+import klj.project.strategyPattern.strategy.SendOuterNoticeEmail;
 import klj.project.web.dto.user.UserTrainerAppplyDto;
 import klj.project.web.dto.user.UserTrainerResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +21,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserTrainerService {
 
     private final UserTrainerRepository userTrainerRepository;
-    private final NoticeService noticeService;
     private final UserRepository userRepository;
+    private final NoticeService noticeService;
+    private final EmailService emailService;
 
     public UserTrainerResponseDto userTrainerApply(User loginUser, UserTrainerAppplyDto userTrainerApplyDto){
         String employmentHistoryPeriod = userTrainerApplyDto.getEmploymentHistoryPeriod();
@@ -32,10 +32,10 @@ public class UserTrainerService {
         String email = userTrainerApplyDto.getEmail();
 
         Trainer trainer =null;
-        if(loginUser.getTrainer() == null){
+        if(loginUser.getTrainer() == null){ //트레이너 신청을 한번도 신청하지않았다면
             trainer = Trainer.createTrainer(loginUser, employmentHistoryPeriod, trainPlace, phoneNumber, email,true);
             userTrainerRepository.save(trainer);
-        }else {
+        }else { //트레이너 신청을 신청했다면
             trainer = loginUser.getTrainer();
             trainer.updateTrainerProfile(employmentHistoryPeriod, trainPlace, phoneNumber, email);
             userTrainerRepository.save(trainer);
@@ -48,12 +48,20 @@ public class UserTrainerService {
         loginUser.applyTrainer(trainer);
         userRepository.save(loginUser);
 
-        // 알림 전송
+        // 알림 전송 콘텍스트 생성
         NoticeContext noticeContext = new NoticeContext();
+
         // 관리자 전송 알림
-        noticeContext.setStrageyNoticeInner(new sendInnerNoticeAdmin(noticeService,userRepository));
+        noticeContext.setStrageyNoticeInner(new SendInnerNoticeAdmin(noticeService,userRepository));
         noticeContext.sendInnerNotice(loginUser, null, "notice01");
 
+        // 이메일 발송
+        String htmlString = ""; // htmltemplate(이메일 본문 내용)
+        String htmlTitleString = "klj-project "; // 이메일 발송 제목
+
+        noticeContext.setStrageyNoticeOuter(new SendOuterNoticeEmail(emailService));
+        noticeContext.sendOuterNotice(loginUser, htmlString, htmlTitleString);
+        
         return userTrainerResponseDto;
     }
 
