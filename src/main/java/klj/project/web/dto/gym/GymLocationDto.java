7c2966adc2d4df97f55aaec9d6aea3484f4c63f0
jakelee0424 <qkql006@gmail.com>
2024-04-22 18:37:59 +0900

    map edit LJK

--- src/main/java/klj/project/web/dto/gym/GymLocationDto.java
+++ src/main/java/klj/project/web/dto/gym/GymLocationDto.java
@@ -1,10 +1,10 @@
 package klj.project.web.dto.gym;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
-@JsonAutoDetect
+@Data
 public class GymLocationDto {
 
     private String address;
